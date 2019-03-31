695c01b2685192b25530f404f0c6ea8e688f09b1
Stephanos Malikotsinas <stephanos@malikotsinas.de>
2019-03-31 16:16:24 +0200

    wip onclick, already showing work

--- app.js
+++ app.js
@@ -3,10 +3,29 @@ var page = {
         logos: [].slice.call(document.querySelectorAll('.flip-container')),
         main: document.querySelector('main')
     },
-    fn: {}
+    fn: {},
+    data: {
+        details: {
+            bigBonsai: {title: 'Big Bonsai Games', description: 'Hallo ich bin LUkaseder fam'},
+            dutchHouse: {title: 'Dutch House', description: 'Hallo ich bin LUkaseder fam'},
+            graffitiPaintbar: {title: 'Graffiti Paintbar', description: 'Hallo ich bin LUkaseder fam'},
+            itlantian: {title: 'Itlantian', description: 'Hallo ich bin LUkaseder fam'},
+            kstm: {title: 'KSTM', description: 'Hallo ich bin LUkaseder fam'},
+            lukaseder: {title: 'Lukaseder Fotografie', description: 'Hallo ich bin LUkaseder fam'},
+            mokka: {title: 'Mokka Café', description: 'Hallo ich bin LUkaseder fam'},
+            mytikas: {title: 'Mytikas', description: 'Hallo ich bin LUkaseder fam'},
+            parallela: {title: 'Parallela', description: 'Hallo ich bin LUkaseder fam'},
+            passionPodcast: {title: 'Passion Podcast', description: 'Hallo ich bin LUkaseder fam'},
+            pulsebox: {title: 'Pulse Box', description: 'Hallo ich bin LUkaseder fam'},
+            racedevil: {title: 'Race Devil', description: 'Hallo ich bin LUkaseder fam'},
+            tgf: {title: 'TGF - Thomas Gottuck Fotografie', description: 'Hallo ich bin LUkaseder fam'},
+            umcd: {title: 'UMCD - Ulrich Müller Consulting und Dienste', description: 'Hallo ich bin LUkaseder fam'},
+            winkler: {title: 'Winkler Hausgerätetechnik', description: 'Hallo ich bin LUkaseder fam'}
+        }
+    }
 };
 
-page.fn.createAdditionalInfo = function(data, container) {
+page.fn.createAdditionalInfo = function(data, name, container) {
 
     var left = document.createElement('div');
     left.className = 'left';
@@ -16,8 +35,8 @@ page.fn.createAdditionalInfo = function(data, container) {
     logoContainerTop.className = 'logo-inside';
 
     var img1 = document.createElement('img');
-    img1.src = 'icons/' + data.name + '_logo_100-01.svg';
-    img1.alt = data.name + ' Logo';
+    img1.src = 'icons/' + name + '_logo_100-01.svg';
+    img1.alt = name + ' Logo';
 
     logoContainerTop.appendChild(img1);
 
@@ -26,8 +45,8 @@ page.fn.createAdditionalInfo = function(data, container) {
     logoContainerBottom.className = 'logo-inside';
 
     var img2 = document.createElement('img');
-    img2.src = 'icons/' + data.name + '_logo_alt_100-01.svg';
-    img2.alt = data.name + ' Logo';
+    img2.src = 'icons/' + name + '_logo_alt_100-01.svg';
+    img2.alt = name + ' Logo';
 
     logoContainerBottom.appendChild(img2);
 
@@ -65,8 +84,6 @@ page.fn.createAdditionalInfo = function(data, container) {
 
 page.dom.main.addEventListener('click', function(e) {
     if (e.target && e.target.className && e.target.className.indexOf('flip-container') > -1) {
-        e.target.dataset.details = JSON.stringify({name: 'lukaseder', title: 'lukaseder', description: 'Hallo ich bin LUkaseder fam'});
-        // TODO: hier weitermachen
-        e.target.insertAdjacentElement(page.fn.createAdditionalInfo(JSON.parse(e.target.dataset.details), undefined));
+        e.target.parentNode.insertBefore(page.fn.createAdditionalInfo(page.data.details[e.target.dataset.name], e.target.dataset.name, undefined), e.target.nextSibling);
     }
 })
